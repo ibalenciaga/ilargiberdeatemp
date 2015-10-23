5039df0fd8c74350d92b94b9b3223ed7043e4706
ibai <ibai@ibai-portatil.(none)>
2015-10-23 20:43:11 +0200

    taldea

---
+++ js/taldea.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  $("#retrato1").mouseover(function(event) {
+    $("#p1").slideDown();
+    
+  });
+
+  $("#retrato1").mouseleave(function(event) {
+    $("#p1").slideUp();
+    
+  });
+
+  $("#retrato2").mouseover(function(event) {
+    $("#p2").slideDown();
+    
+  });
+
+  $("#retrato2").mouseleave(function(event) {
+    $("#p2").slideUp();
+    
+  });
+
+  $("#retrato3").mouseover(function(event) {
+    $("#p3").slideDown();
+    
+  });
+
+  $("#retrato3").mouseleave(function(event) {
+    $("#p3").slideUp();
+    
+  });
+})
